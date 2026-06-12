e99137be763379a93b1fb23b9efbd30026d831e8
Ayush Kumar <ayushkumarranjan123@gmail.com>
2026-06-12 22:33:47 +0530

    add scrolling for meetings details

--- src/component/meetingComponents/meetingTable.jsx
+++ src/component/meetingComponents/meetingTable.jsx
@@ -6,26 +6,33 @@ import MeetingDetails from "./meetingDetails";
 const MeetingTable=({meets,setMeets})=>{
     return (
         <>
-          <table class="table table-hover gap-5">
-                <thead>
-                    <tr>
-                        <th className='px-4' scope="col">#</th>
-                        <th className='px-4'scope="col">Date</th>
-                        <th className='px-4' scope="col">Time</th>
-                        <th className={`${styles.description} px-4`} scope="col" >Description</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {meets.map((meet,idx)=>{
-                        return (
-                            <>
-                              <MeetingDetails meet={meet} idx={idx} meets={meets} setMeets={setMeets}></MeetingDetails>   
-                            </>
+            <div style={{height:'60vh', overflowY:'auto',width:'45vw'}}>
+                <table class="table table-hover gap-5" style={{ borderCollapse: 'collapse', width: '100%' }}>
+                    <thead style={{ position: 'sticky', top: 0, backgroundColor: 'white', zIndex: 1 }}>
+                        <tr>
+                            <th className='px-4' scope="col">S.No.</th>
+                            <th className='px-4'scope="col">Date</th>
+                            <th className='px-4' scope="col">Time</th>
+                            <th className='px-4' scope="col" >Description</th>
+                            <th className='px-2' scope="col" ></th>
+                            <th className='px-2' scope="col" ></th>
                             
-                        ) 
-                    })}
-                </tbody>
-            </table>
+
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {meets.map((meet,idx)=>{
+                            return (
+                                <>
+                                    <MeetingDetails  meet={meet} idx={idx} meets={meets} setMeets={setMeets}></MeetingDetails>
+
+                                </>
+                                
+                            ) 
+                        })}
+                    </tbody>
+                </table>
+            </div> 
         </>
     )
 }
